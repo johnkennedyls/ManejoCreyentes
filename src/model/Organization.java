43fe474f8652e1d116807d354ff3201365ff37c3
johnkennedyls <jkls1998@gmail.com>
2021-06-18 14:22:20 -0500

    read csv

--- src/model/Organization.java
+++ src/model/Organization.java
@@ -16,40 +16,5 @@ public class Organization {
 	}
 	
 	
-	public static void leerArchivo(String ruta) {
-
-     	String dato;
-     	
-        try {
-            File archivo = new File(ruta);
-            Scanner myReader = new Scanner(archivo);
-            String[] list = myReader.nextLine().split(";");
-            int[][] myMatrix = new int[list.length-1][list.length-1];
-            int row = 0;
-            String[] info;
-            while (myReader.hasNextLine()) {
-                dato = myReader.nextLine();
-                info = dato.split(";");
-                System.out.println("Tamaño info: " + info.length);
-                for (int i = 1; i < list.length; i++) {
-                    myMatrix[row][i-1] = Integer.parseInt(info[i]);
-                }
-                row++;               
-            }
-            
-            for (int j = 0; j < list.length-1; j++){ 
-                for (int k = 0; k < list.length-1; k++){ 
-                    System.out.print(myMatrix[j][k]+" "); 
-                } 
-                System.out.println(""); 
-            }
-            
-        } catch (FileNotFoundException ex) {
-             System.out.println("El archivo no existe");
-        }
-
-      
-     }
-	
 }
 

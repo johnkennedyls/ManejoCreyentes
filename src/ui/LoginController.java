ee8d49bb9f4fe7e78b4c8b61df3a399a8f335296
SeanQO <seanquintero3@gmail.com>
2021-06-18 22:38:11 -0500

    added principal window and its controller

--- src/ui/LoginController.java
+++ src/ui/LoginController.java
@@ -1,10 +1,16 @@
 package ui;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 	@FXML
@@ -15,7 +21,30 @@ public class LoginController {
 
     @FXML
     void login(ActionEvent event) {
+    	try {
+			
+    		openPrincipalWindow();
+    		
+    		passwordField.getScene().getWindow().hide();
+    		
+		} catch (IOException ioException) {
+			// TODO: handle exception
+			ioException.printStackTrace();
+			
+		}
+    	
     	verifyLogin();
+    }
+    
+    private void openPrincipalWindow() throws IOException{
+    	Parent root = FXMLLoader.load(getClass().getResource("fxml/principal.fxml"));
+		Scene scene = new Scene(root);
+		Stage secondaryStage = new Stage();
+		secondaryStage.setScene(scene);
+		secondaryStage.setTitle("Acceder");
+		secondaryStage.setResizable(false);
+		secondaryStage.show();
+		
     }
     
     private boolean verifyLogin() {

9f8c1e8178abdb753332c7a5f6899923e0697d0c
SeanQO <seanquintero3@gmail.com>
2021-06-26 21:39:12 -0500

    added info button in login window, basic test to show info

--- src/ui/LoginController.java
+++ src/ui/LoginController.java
@@ -6,10 +6,15 @@ import exceptions.InvalidUserException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Church;
 
@@ -78,6 +83,38 @@ public class LoginController {
     	
     	return false;*/
     	return true;
+    }
+    
+    @FXML
+    void developersInfo(ActionEvent event) {
+    	Stage popupwindow=new Stage();
+        
+    	popupwindow.initModality(Modality.APPLICATION_MODAL);
+    	popupwindow.setTitle("Desarrolladores");
+    	      
+    	      
+    	Label label1= new Label("Info1\n" + "Info2\n" + "Info3\n" + "Info4\n" + "" + "");
+    	      
+    	     
+    	Button button1= new Button("Cerrar");
+    	     
+    	     
+    	button1.setOnAction(e -> popupwindow.close());
+    	     
+    	     
+
+    	VBox layout= new VBox(10);
+    	     
+    	      
+    	layout.getChildren().addAll(label1, button1);
+    	      
+    	layout.setAlignment(Pos.CENTER);
+    	      
+    	Scene scene1= new Scene(layout, 300, 250);
+    	      
+    	popupwindow.setScene(scene1);
+    	      
+    	popupwindow.showAndWait();
     }
 
 }
